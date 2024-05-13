a7d53f2dcba55d5a8f79e9cd1e3997c1f77e0ac9
sattorovsharofiddin <cyberpancher10@gmail.com>
2024-05-13 15:12:43 +0500

    add changes models

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,26 +1,22 @@
-from django.db import models
-from django.db.models import Model
+from mptt.fields import TreeForeignKey
+from mptt.models import MPTTModel
+from django.db.models import ForeignKey, CASCADE, URLField, FileField
 
 from shared.models import BaseModel
 
 
-class Category(BaseModel):
-    def __str__(self):
-        return self.name
-
-
-class Type(BaseModel):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+class Category(MPTTModel, BaseModel):
+    parent = TreeForeignKey('self', on_delete=CASCADE, null=True, blank=True, related_name='children')
 
     def __str__(self):
         return self.name
 
 
 class Media(BaseModel):
-    url = models.URLField()
-    data = models.FileField(upload_to='media/')
+    url = URLField()
+    data = FileField(upload_to='media/')
 
-    type = models.ForeignKey(Type, on_delete=models.CASCADE)
+    type = ForeignKey(Category, on_delete=CASCADE)
 
     def __str__(self):
         return self.name
